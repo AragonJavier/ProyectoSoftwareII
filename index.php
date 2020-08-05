078d381b2e58c82701fb346520836549a12b6ef6
Alexander <aragonjavier07@gmail.com>
2020-08-05 13:34:25 -0600

    Initial commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+</head>
+
+<body>
+    <form action="login.php" method="post">
+        <label for="">Usuario</label>
+        <input type="text" name="usuario" id=" usuario" placeholder="Usuario">
+        <label for="">Pasword</label>
+        <input type="password" name="password" id="password" palceholder="Password">
+    </form>
+</body>
+
+</html>
