35f59e2df67a8ab0c099b2dd12392fa2456796d0
Alexander <aragonjavier07@gmail.com>
2020-08-05 13:49:36 -0600

    V1.0

--- index.php
+++ index.php
@@ -8,11 +8,12 @@
 </head>
 
 <body>
-    <form action="login.php" method="post">
+    <form action="login.php" method="post" id="form">
         <label for="">Usuario</label>
         <input type="text" name="usuario" id=" usuario" placeholder="Usuario">
         <label for="">Pasword</label>
-        <input type="password" name="password" id="password" palceholder="Password">
+        <input type="password" name="password" id="password" placeholder="Password">
+        <input type="submit" value="Iniciar Sesión">
     </form>
 </body>
 
